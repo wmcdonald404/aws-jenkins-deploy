8d6d550f47fe8a971a22cd7b880931cc5542f249
Will McDonald <wmcdonald@gmail.com>
2025-07-20 16:32:57 +0000

    Add VPC config for sharedsvc VPC.

---
+++ tf/env/sharedsvc/vpc/main.tf
@@ -0,0 +1,21 @@
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+
+module "vpc" {
+  source = "../../../modules/terraform-aws-modules/vpc/aws/"
+
+  name = "sharedsvc-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+  # create_egress_only_igw = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "sharedsvc"
+  }
+}
